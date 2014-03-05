56597369fe09c0dd29123cabf1bffcb6c68fb73b
gregstallings <gregstallings@gmail.com>
2014-03-04 23:15:58 -0800

    Add dir manipulation helper functions

--- shrc/manipulation.sh
+++ shrc/manipulation.sh
@@ -9,21 +9,40 @@ alias ln='ln -v'
 # Most common options for symbolic links
 alias lns='ln -sfvn'
 alias mkd='mkdir -pv'
+alias mk='mkd'
 # Prompt before overwriting
 alias mv='mv -vi'
 alias rm='rm -v'
 alias rmf='rm -rfv'
 alias to='touch'
 
+# Copy the current directory into the given directory
+ccd() {
+  local current_dir="$(pwd)"
+  local target_dir=$(cd $(dirname "$1"); pwd)/$(basename "$1")
+  cd .. && cp -Rvi "$current_dir" "$target_dir"
+  echo
+  c "$current_dir"
+}
+
 # Create a new directory and enter it
-mcd() {
+mkcd() {
   mkdir -pv "$@" && cd "$@"
+}
+
+# Move the current directory into the given directory
+mvcd() {
+  local current_dir="$(pwd)"
+  local target_dir=$(cd $(dirname "$1"); pwd)/$(basename "$1")
+  cd .. && mv -vi "$current_dir" "$target_dir"
+  echo
+  c "$target_dir"
 }
 
 # Remove the current directory and traverse to the enclosing directory
 rcd() {
-  local dir_to_remove="$(pwd)"
-  cd .. && rm -rfv "$dir_to_remove"
+  local current_dir="$(pwd)"
+  cd .. && rm -rfv "$current_dir"
   echo
   c .
 }
