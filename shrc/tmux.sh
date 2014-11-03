b599d36d346cba803573ce9a568809f0c847e3f0
gregstallings <gregstallings@gmail.com>
2014-11-02 21:11:34 -0800

    Increment cwd to find free tmux session name

--- shrc/tmux.sh
+++ shrc/tmux.sh
@@ -1,11 +1,33 @@
 alias tma='tmux attach'
 alias tmi='tmux info | less -iFNQRX'
 
-# Start a new tmux session with the cwd basename, `tmux` otherwise
+tmux_has_session() {
+  tmux has-session -t "$1" 2>/dev/null
+}
+
+# Starts a new tmux session with the cwd basename, `tmux` otherwise
 tm() {
   if [ $# -eq 0 ]; then
     local cwd_basename="$(basename "$(pwd)")"
-    tmux new-session -s "$cwd_basename" -n "$cwd_basename"
+
+    # Try $cwd_basename + "-\d" until an unused session name is found
+
+    local session_name_test="$cwd_basename"
+    local has_session=0
+    if $(tmux_has_session "$session_name_test"); then
+      has_session=1
+    else
+      has_session=0
+    fi
+
+    local count=1
+    while [ "$has_session" ]; do
+      session_name_test="${cwd_basename}-${count}"
+      count=$(( count + 1 ))
+      $(tmux_has_session "$session_name_test") || break
+    done
+
+    tmux new-session -s "$session_name_test" -n "$session_name_test"
   else
     tmux "$@"
   fi
