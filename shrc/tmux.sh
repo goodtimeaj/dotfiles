c74701072178f713f797bcaf1390c22efb8b1351
gregstallings <gregstallings@gmail.com>
2014-11-02 21:40:37 -0800

    Use current repo name for new tmux sessions

--- shrc/tmux.sh
+++ shrc/tmux.sh
@@ -9,25 +9,32 @@ tmux_has_session() {
 tm() {
   if [ $# -eq 0 ]; then
     local cwd_basename="$(basename "$(pwd)")"
+    local session_name_target="$cwd_basename"
+
+    # Set the session name to the repo name when within a git repo
+    local git_working_dir="$(git rev-parse --show-toplevel)"
+    if [ $git_working_dir ]; then
+      session_name_target="$(basename "$git_working_dir")"
+    fi
 
     # Try $cwd_basename + "-\d" until an unused session name is found
 
-    local session_name_test="$cwd_basename"
     local has_session=0
-    if $(tmux_has_session "$session_name_test"); then
+    if $(tmux_has_session "$session_name_target"); then
       has_session=1
     else
       has_session=0
     fi
 
     local count=1
+    local session_name_test="$session_name_target"
     while [ "$has_session" ]; do
-      session_name_test="${cwd_basename}-${count}"
-      count=$(( count + 1 ))
       $(tmux_has_session "$session_name_test") || break
+      session_name_test="${session_name_target}-${count}"
+      count=$(( count + 1 ))
     done
 
-    tmux new-session -s "$session_name_test" -n "$session_name_test"
+    tmux new-session -s "$session_name_test" -n "$cwd_basename"
   else
     tmux "$@"
   fi
