d4f00d404ed11a5c443df7b85d7f62571a36680e
gregstallings <gregstallings@gmail.com>
2014-11-02 18:49:07 -0800

    `tm` starts new tmux session with cwd basename if no args

--- shrc/tmux.sh
+++ shrc/tmux.sh
@@ -1,2 +1,11 @@
-alias tm='tmux'
 alias tmi='tmux info | less -iFNQRX'
+
+# Start a new tmux session with the cwd basename, `tmux` otherwise
+tm() {
+  if [ $# -eq 0 ]; then
+    local cwd_basename="$(basename "$(pwd)")"
+    tmux new-session -s "$cwd_basename" -n "$cwd_basename"
+  else
+    tmux "$@"
+  fi
+}
