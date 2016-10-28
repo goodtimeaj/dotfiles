da6f941e91f468fbcf607e654b5842271eac2adf
gregstallings <gregstallings@gmail.com>
2016-10-28 17:00:42 -0500

    Update rbenv shims path

--- shrc/zpath.sh
+++ shrc/zpath.sh
@@ -8,7 +8,7 @@ PATH="/usr/local/sbin:${PATH}"
 PATH="/usr/local/bin:${PATH}"
 
 if [ -d "${RBENV_ROOT}" ]; then
-  PATH="${RBENV_ROOT}/bin:${PATH}"
+  PATH="${RBENV_ROOT}/shims:${PATH}"
   eval "$(rbenv init -)"
 fi
 
